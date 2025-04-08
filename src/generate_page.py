d72f128ca4bbf4c83db0ff1e7b5a317259314441
dogukan-polat <dogukan.plt.96@gmail.com>
2025-04-08 14:13:33 +0300

    Web page created.

--- src/generate_page.py
+++ src/generate_page.py
@@ -13,27 +13,27 @@ def get_content(path):
     with open(path) as f:
         return f.read()
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(base_path, from_path, template_path, dest_path):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}:")
     md_content = get_content(from_path)
     template_content = get_content(template_path)
     converted_md = markdown_to_html_node(md_content).to_html()
     #print(converted_md)  # Print its structure to inspect nodes.
     title = extract_title(md_content)
-    html_page = template_content.replace("{{ Title }}", title).replace("{{ Content }}", converted_md)
+    html_page = template_content.replace("{{ Title }}", title).replace("{{ Content }}", converted_md).replace('href="/', f'href="{base_path}').replace('src="/', f'src="{base_path}')
     if not os.path.exists(os.path.dirname(dest_path)):
         os.makedirs(os.path.dirname(dest_path))
     with open(dest_path, "w") as f:
         f.write(html_page)
 
-def generate_pages_recursively(dir_path_content, template_path, dest_dir_path):#MD files must turn to HTML.
+def generate_pages_recursively(base_path, dir_path_content, template_path, dest_dir_path):#MD files must turn to HTML.
     filelist = os.listdir(dir_path_content)
     for filename in filelist:
         src_path = os.path.join(dir_path_content, filename)
         dst_path = os.path.join(dest_dir_path,filename)
         if os.path.isfile(src_path):
             dst_path = os.path.join(os.path.dirname(dst_path), "index.html")
-            generate_page(src_path, template_path, dst_path)
+            generate_page(base_path, src_path, template_path, dst_path)
         else:
             os.mkdir(dst_path)
-            generate_pages_recursively(src_path, template_path, dst_path)
+            generate_pages_recursively(base_path, src_path, template_path, dst_path)
--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import os, shutil
+import sys
 from textnode import *
 from copy_statics import copy_paste
 from generate_page import generate_pages_recursively
@@ -6,15 +7,16 @@ from generate_page import generate_pages_recursively
 
 def main():
     static_dest = "static/"
-    public_dest = "public/"
-    
+    base_path = "/"
+    if len(sys.argv) == 2:
+        base_path = sys.argv[1]
 
-    if os.path.exists(public_dest):
-        shutil.rmtree(public_dest)
+    if os.path.exists("docs/"):
+        shutil.rmtree("docs/")
 
-    copy_paste(static_dest, public_dest)
+    copy_paste(static_dest, "docs/")
 
     ##generate_page("content/index.md", "template.html", "public/index.html")
-    generate_pages_recursively("content/", "template.html", "public/")
+    generate_pages_recursively(base_path, "content/", "template.html", "docs/")
 
 main()
